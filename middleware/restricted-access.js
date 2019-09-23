e0346f4d9d4f8facc5328af90b510928260d1681
Adnan <adnan.chowdhury@outlook.com>
2019-09-23 15:58:02 -0500

    Restricted Middleware setup
    Co-authored-by: John <cjd8801@gmail.com>

---
+++ middleware/restricted-access.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const secrets = require("../config/jwt.js");
+
+module.exports = function restricted(req, res, next) {
+  const token = req.headers.token;
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        // token is invalid
+        console.log(err);
+        res
+          .status(401)
+          .json({ message: "Your token is invalid. You are unauthorized." });
+      } else {
+        // good token
+        req.username = { username: decodedToken.username };
+        req.userId = decodedToken.subject;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Get a token and try again. :-)" });
+  }
+};
