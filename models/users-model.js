4a07aeb6e02f738f081d234bf0bbee61be6add3e
Adnan <adnan.chowdhury@outlook.com>
2019-09-24 16:01:44 -0500

    Login endpoint created!
    Co-authored-by: John <cjd8801@gmail.com>

---
+++ models/users-model.js
@@ -0,0 +1,38 @@
+const db = require("../database/dbConfig");
+
+module.exports = { add, find, findBy, findById, update, remove };
+
+function add(user) {
+  return db("users")
+    .insert(user, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+}
+
+function find() {
+  return db("users").select("*");
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+function remove(id) {
+  return db("users")
+    .where({ id })
+    .del();
+}
+
+function update(data, id) {
+  return db("users")
+    .where({ id })
+    .update(data);
+}
--- routes/auth-router.js
+++ routes/auth-router.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const Users = require("../models/users-model");
 const secrets = require("../config/jwt");
 
 // router.post("/register", (req, res) => {
@@ -19,24 +20,24 @@ const secrets = require("../config/jwt");
 //     });
 // });
 
-// router.post("/login", (req, res) => {
-//   // implement login
-//   let { username, password } = req.body;
+router.post("/login", (req, res) => {
+  // implement login
+  let { username, password } = req.body;
 
-//   Users.findBy({ username })
-//     .first()
-//     .then(user => {
-//       if (user && bcrypt.compareSync(password, user.password)) {
-//         const token = generateToken(user);
-//         res.status(200).json({ token });
-//       } else {
-//         res.status(401).json({ message: "Invalid Credentials" });
-//       }
-//     })
-//     .catch(error => {
-//       res.status(500).json(error);
-//     });
-// });
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
 function generateToken(user) {
   const payload = {
