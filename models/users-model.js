6fa6b09f562fc0fbeb766609166326a048c45a17
Adnan <adnan.chowdhury@outlook.com>
2019-09-25 13:21:01 -0500

    Added Endpoints to access User & Bucketlists,
    Co-authored-by: John <cjd8801@gmail.com>

--- models/users-model.js
+++ models/users-model.js
@@ -1,6 +1,14 @@
 const db = require("../database/dbConfig");
 
-module.exports = { add, find, findBy, findById, update, remove };
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  remove,
+  findUserWithData
+};
 
 function add(user) {
   return db("users")
@@ -35,4 +43,28 @@ function update(data, id) {
   return db("users")
     .where({ id })
     .update(data);
+}
+
+function findUserWithData(id) {
+  return db("users")
+    .select("user_id", "username", "email")
+    .where("user_id", id)
+    .first()
+    .then(user => {
+      let myUser = { ...user };
+      return db("bucketLists as BL")
+        .join(
+          "bucketListsItems as BLI",
+          "BL.bucket_list_id",
+          "BLI.bucket_list_id"
+        )
+        .where("bucket_list_user_id", id)
+        .then(bucketlist => {
+          let sharedList = bucketlist.filter(e => e.private === 0);
+          let privateList = bucketlist.filter(e => e.private === 1);
+          myUser.sharedList = sharedList;
+          myUser.privateList = privateList;
+          return myUser;
+        });
+    });
 }
