ce7c33969e9ebc525078257934fc46ad338ce451
Adnan <adnan.chowdhury@outlook.com>
2019-09-26 12:34:56 -0500

    testing new seeds for postgres

--- database/seeds/01_user.js
+++ database/seeds/01_user.js
@@ -4,11 +4,13 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex("users").insert([
     {
+      user_id: 1,
       username: "bill124",
       password: bcrypt.hashSync("password123"),
       email: "billy@wildwest.com"
     },
     {
+      user_id: 2,
       username: "bucketboy",
       password: bcrypt.hashSync("bucket"),
       email: "bucky@buck.com"
