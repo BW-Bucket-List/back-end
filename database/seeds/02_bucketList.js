cfe315ede558255ca5030140201ed07e79aa4ff3
Adnan <adnan.chowdhury@outlook.com>
2019-09-26 12:46:55 -0500

    try old seeds

--- database/seeds/02_bucketList.js
+++ database/seeds/02_bucketList.js
@@ -2,25 +2,25 @@ exports.seed = function(knex) {
   // Inserts seed entries
   return knex("bucketLists").insert([
     {
-      bucket_list_id: 1,
+      //bucket_list_id: 1,
       bucket_list_name: "Running with the bulls",
       bucket_list_user_id: 1,
       private: true
     },
     {
-      bucket_list_id: 2,
+      //bucket_list_id: 2,
       bucket_list_name: "Bucky's Top 3 games",
       bucket_list_user_id: 2,
       private: false
     },
     {
-      bucket_list_id: 3,
+      //bucket_list_id: 3,
       bucket_list_name: "Bucky's Favorite Games",
       bucket_list_user_id: 2,
       private: true
     },
     {
-      bucket_list_id: 4,
+      //bucket_list_id: 4,
       bucket_list_name: "Bucky's Scuba Diving Goals",
       bucket_list_user_id: 2,
       private: false
